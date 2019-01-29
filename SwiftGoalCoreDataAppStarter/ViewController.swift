8672dc9836eba005b67288e4fa25f1cb74e433ef
a <eahilendran@gmail.com>
2019-01-29 23:08:57 +0530

    first initial commit

---
+++ SwiftGoalCoreDataAppStarter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftGoalCoreDataAppStarter
+//
+//  Created by MacBook on 1/29/19.
+//  Copyright © 2019 Ahil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
