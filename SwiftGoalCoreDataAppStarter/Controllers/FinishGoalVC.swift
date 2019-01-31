f3a98c1a2965247d5adb789707f32e792bdecb84
a <eahilendran@gmail.com>
2019-02-01 01:03:13 +0530

    Save data to CoreData

--- SwiftGoalCoreDataAppStarter/Controllers/FinishGoalVC.swift
+++ SwiftGoalCoreDataAppStarter/Controllers/FinishGoalVC.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class FinishGoalVC: UIViewController {
+let appDelegate = UIApplication.shared.delegate as? AppDelegate
 
+class FinishGoalVC: UIViewController, UITextFieldDelegate {
+
+    private var goalDescription: String?
+    private var goalType: GoalType?
     
-    var goalDescription: String?
-    var goalType: GoalType?
     @IBOutlet weak var finishGoalButton: UIButton!
     @IBOutlet weak var pointsTextField: UITextField!
     
@@ -25,14 +27,47 @@ class FinishGoalVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        finishGoalButton.bindToKeyboard()
+        pointsTextField.delegate = self
     }
     @IBAction func finishButtonTapped(_ sender: Any) {
-        
+        if pointsTextField.text != "" {
+            self.save { (complete) in
+                if complete {
+                    dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     @IBAction func backButtonTapped(_ sender: Any) {
         dismissDetail()
     }
     
+    func save(completion: (_ finished: Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        
+        guard let points = pointsTextField.text else{return}
+        let goal = Goal(context: managedContext)
+        
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType!.rawValue
+        goal.goalCompleteValue = Int32(points)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+            try managedContext.save()
+            print("Successfully saved data.")
+            completion(true)
+        } catch {
+            debugPrint("Could not save: \(error.localizedDescription)")
+            completion(false)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        pointsTextField.resignFirstResponder()
+    }
+    
+
 }
